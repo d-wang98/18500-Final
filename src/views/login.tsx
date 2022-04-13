e226d89781f9d829a49e4114e82deea1cd79bc9a
d-wang98 <dwang3@andrew.cmu.edu>
2022-04-13 11:27:15 -0400

    merge fixes

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -53,7 +53,7 @@ export default function Login({isLoggedIn}) {
       <form>
         <label>Email or username</label>
         <label>Password</label>
-        {myStoreState.wallet.isSignedIn() ? <Button2 onClick={handleLogout}>Log out</Button2> : <Button2 onClick={handleLogin}>Log in</Button2>}
+        {myStoreState.wallet.isSignedIn() ? <Button onClick={handleLogout}>Log out</Button> : <Button onClick={handleLogin}>Log in</Button>}
       </form>
       <Link to="/">
         <Button variant="contained">Back</Button>
